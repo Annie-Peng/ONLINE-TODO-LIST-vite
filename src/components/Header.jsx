7918528ddee047bf20348f417629f0bd05d9a553
Annie-Peng <djs910532@gmail.com>
2023-08-29 15:48:02 +0800

    style-update logo import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import { logo } from '../images/cover/logo.png'
+import logo from '../images/cover/logo.png'
 
 export default function Header() {
   return (
     <nav className="flex justify-between items-center mt-2 ms-[34px] me-[32px]">
       <img src={logo} className="w-[243px] h-[38px]" />
-      <h2 className="ms-auto font-bold">王小明的代辦</h2>
+      <h2 className="npm ms-auto font-bold">王小明的代辦</h2>
       <button type='button' className="ms-3">登出</button>
     </nav>
   )
